06266c168cd54d65d64940410947c73381af00fd
arcmichaellogisticsllc-cloud <arcmichaellogistics.llc@gmail.com>
2025-10-02 12:38:03 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('dashboard', function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+use App\Http\Controllers\PODController;
+
+Route::post('/loads/{load}/pod/submit', [PODController::class, 'submit'])
+    ->name('loads.pod.submit');
