aced4f578a0fe2fd6e4d81c9e664ef6a4045a3f7
arcmichaellogisticsllc-cloud <arcmichaellogistics.llc@gmail.com>
2025-10-04 18:35:47 -0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -5,34 +5,48 @@ return [
     'default' => env('MAIL_MAILER', 'smtp'),
 
     'mailers' => [
+
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
-            'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', null),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
+
+        'ses' => ['transport' => 'ses'],
+        'mailgun' => ['transport' => 'mailgun'],
+        'postmark' => ['transport' => 'postmark'],
 
         'log' => [
             'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
         ],
 
-        'array' => [
-            'transport' => 'array',
+        'array' => ['transport' => 'array'],
+
+        'failover' => [
+            'transport' => 'failover',
+            'mailers' => [
+                'smtp',
+                'log',
+            ],
         ],
     ],
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@paytread.com'),
         'name' => env('MAIL_FROM_NAME', 'PayTread'),
     ],
 
-  'markdown' => [
-    'theme' => 'paytread',
-    'paths' => [
-        resource_path('views/vendor/mail'),
+    'markdown' => [
+        'theme' => 'paytread',
+        'paths' => [
+            resource_path('views/vendor/mail'),
+        ],
     ],
-],
 
+];
