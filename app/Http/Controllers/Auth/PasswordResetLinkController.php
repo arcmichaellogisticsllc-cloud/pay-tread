7a49e24c8341168489e9985face66b555be7f86b
arcmichaellogisticsllc-cloud <arcmichaellogistics.llc@gmail.com>
2025-10-13 21:04:29 -0500

    Update PasswordResetLinkController.php

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -1,41 +1,18 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-use Inertia\Inertia;
-use Inertia\Response;
+use App\Http\Controllers\Controller;
 
 class PasswordResetLinkController extends Controller
 {
-    /**
-     * Show the password reset link request page.
-     */
-    public function create(Request $request): Response
+    public function store(Request $request)
     {
-        return Inertia::render('auth/ForgotPassword', [
-            'status' => $request->session()->get('status'),
-        ]);
-    }
+        $request->validate(['email' => ['required','email']]);
 
-    /**
-     * Handle an incoming password reset link request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'email' => 'required|email',
-        ]);
+        $status = Password::sendResetLink($request->only('email'));
 
-        Password::sendResetLink(
-            $request->only('email')
-        );
-
-        return back()->with('status', __('A reset link will be sent if the account exists.'));
+        return back()->with('status', __($status));
     }
 }
